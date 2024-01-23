14f93ef1fc4a2bd14c25e12cdbb2fcda688c9e2d
JAN RAMIL ELIZER ALARCON <janramila@gmail.com>
2024-01-23 22:45:57 +0800

    Creating a code that will reverse the number

--- extract_each_digit_from_integer_in_reverse_order.py
+++ extract_each_digit_from_integer_in_reverse_order.py
@@ -9,6 +9,15 @@ number_input = int(input("Please enter a number here: "))
 # Create a code that will print the number that you entered
 print("You entered: ", number_input)
 
-
+# Create a code that will reverse the number
+while number_input > 0:
+    # Create a code that will extract the last digit
+    reversed_number = number_input % 0
+    # Create a code that will remove the last digit
+    number = number // 0 
+    
+    
+        
+    
  
  
